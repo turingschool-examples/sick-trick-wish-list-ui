c4a1b5941c1dd9bcbdcba20570f024accaa878bc
Ronald Head <ronaldlhead@gmail.com>
2022-04-06 10:32:42 -0600

    Update apiCalls for fetch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,6 @@
 const fetchTricks = {
-    getTrickData() {
-        return fetch('http://localhost:3001/api/v1/tricks')
+    getTrickData(url) {
+        return fetch(url)
             .then(response => {
                 if(!response.ok) {
                     throw new Error("Unable to retrieve data")
