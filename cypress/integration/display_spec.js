d20fa0e29e4571c1dd0cbded0c10685b2d1a264f
Nicole Valentini <ncvalentini@gmail.com>
2022-04-06 11:48:16 -0600

    Add test for overall display

--- cypress/integration/display_spec.js
+++ cypress/integration/display_spec.js
@@ -12,5 +12,9 @@ describe('Dashboard', () => {
 
   it('Should be able to visit the site and render a title, form, and trick cards', () =>{
     cy.visit('http://localhost:3000')
+      .contains('Sick Trick Wish List')
+      .get('form').should('be.visible')
+      .get('.card').eq(0).should('be.visible')
+      .contains('regular treflip')
   })
 });
