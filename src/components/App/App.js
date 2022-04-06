d05eb4516f3a859c3cf3e30946a3121f7dd13344
Ronald Head <ronaldlhead@gmail.com>
2022-04-06 09:26:33 -0600

    Inital commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,14 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tricks: []
+    }
+  }
+
+  
   render() {
     return (
       <div className="App">
