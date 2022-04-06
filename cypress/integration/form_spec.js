f4c4f8193a9686e00059f8a618138135a47dd5c7
Nicole Valentini <ncvalentini@gmail.com>
2022-04-06 12:08:40 -0600

    Add test for submitting new trick

--- cypress/integration/form_spec.js
+++ cypress/integration/form_spec.js
@@ -12,4 +12,25 @@ describe('Form', () => {
       .get('.obstacle').select('Rail')
       .should('have.value', "Rail")
   })
+
+  it('Should allow the user to input data, submit that trick, and see trick on dashboard', () =>{
+    cy.visit('http://localhost:3000')
+      .get('form').should('be.visible')
+      .get('.trick').type('Nicole')
+      .should('have.value', 'Nicole')
+      .get('.tutorial').type('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+      .should('have.value', "https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+      .get('.stance').select('Regular')
+      .should('have.value', "Regular")
+      .get('.obstacle').select('Rail')
+      .should('have.value', "Rail")
+      .get('button').click()
+      .get('.card').eq(3).should('be.visible')
+      .contains('Regular Nicole')
+  })
+
+  //Out of time but here's what else I would add:
+
+  //Add a test to make sure the link to the trick video can be clicked on a card, and the user
+  //is directed to a new url. 
 });
