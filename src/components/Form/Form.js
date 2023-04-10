6b92253fedf9d68c434aecff3caea51eab02b1ee
Travis Parks <travisparks0788@gmail.com>
2023-04-10 13:16:48 -0400

    Add information to trick

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,67 +1,75 @@
-import { useState } from "react"
-import "./Form.css"
+import { useState } from "react";
+import "./Form.css";
 
-const Form = ({addTrick}) => {
-  
-  const [stance, setStance] = useState('')
-  const [name, setName] = useState('')
-  const [obstacle, setObstacle] = useState('')
-  const [tutorial, setTutorial] = useState('')
+const Form = ({ addTrick }) => {
+  const [stance, setStance] = useState("");
+  const [name, setName] = useState("");
+  const [obstacle, setObstacle] = useState("");
+  const [tutorial, setTutorial] = useState("");
 
-  const sumbitTrick = (event) => {
-    event.preventDefault()
+  const submitTrick = (event) => {
+    event.preventDefault();
     const newTrick = {
       stance,
       name,
       obstacle,
-      tutorial
-    }
-    addTrick(newTrick)
-    clearInputs()
-  }
+      tutorial,
+    };
+    addTrick(newTrick);
+    clearInputs();
+  };
 
   const clearInputs = () => {
-    setStance('')
-    setName('')
-    setObstacle('')
-    setTutorial('')
-  }
-
-
+    setStance("");
+    setName("");
+    setObstacle("");
+    setTutorial("");
+  };
 
   return (
     <form className="form">
       <label className="label">
         <select name="Choose Your Stance">
-        <option value="" disabled selected>Choose Your Stance</option>
-        <option value="Regular">Regular</option>
+          <option value="" disabled selected>
+            Choose Your Stance
+          </option>
+          <option value="Regular">Regular</option>
           <option value="Switch">Switch</option>
+          onChange={(event) => setStance(event.target.value)}
         </select>
       </label>
-      <input className="input"
+      <input
+        className="input"
         type="text"
         placeholder="Name of Trick"
         name="trick"
         value={name}
-        onChange={event => setName(event.target.value)}
+        onChange={(event) => setName(event.target.value)}
       />
       <label className="label">
         <select name="Choose Your Obstacle">
-          <option value="" disabled selected>Choose Your Obstacle</option>
-          <option value="Regular">Regular</option>
-          <option value="Switch">Switch</option>
+          <option value="" disabled selected>
+            Choose Your Obstacle
+          </option>
+          <option value="Flatground">Flatground</option>
+          <option value="Ledge">Ledge</option>
+          <option value="Rail">Rail</option>
+          <option value="Stairs">Stairs</option>
+          <option value="Pool">Pool</option>
+          onChange={(event) => setObstacle(event.target.value)}
         </select>
       </label>
-      <input className="input"
+      <input
+        className="input"
         type="text"
         placeholder="Link to Tutorial"
         name="tutorial"
         value={tutorial}
-        onChange={event => setTutorial(event.target.value)}
+        onChange={(event) => setTutorial(event.target.value)}
       />
-      <button className="button">Send It!!</button>
+      <button onClick={submitTrick} className="button">Send It!!</button>
     </form>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
--- src/components/Trick/Trick.js
+++ src/components/Trick/Trick.js
@@ -5,7 +5,8 @@ const Trick = ({stance, name, obstacle, tutorial, id}) => {
   return (
     <div className="trick">
       <p>{stance}: {name}</p>
-      <p>{obstacle}</p>
+      <p>Obstacle: {obstacle}</p>
+      <p>Link to Tutorial</p>
       <a href={tutorial}>{tutorial}</a>
     </div>
   )
