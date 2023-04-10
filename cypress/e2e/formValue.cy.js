417bcce01651b64e3ee6aa1ab9d0762147341212
Travis Parks <travisparks0788@gmail.com>
2023-04-10 14:14:28 -0400

    Add starting tests

---
+++ cypress/e2e/formValue.cy.js
@@ -0,0 +1,24 @@
+describe('should see a form when visiting the site that they can fill out', () => {
+  beforeEach(() => {
+    cy.intercept(
+      "GET",
+      "https://localhost:3001/api/v1/tricks",
+      { fixture: "allTricks.json" }
+    );
+    cy.visit("http://localhost:3000");
+  })
+
+  it('should have a form with different fields to fill out', () => {
+    cy.get('.form').contains('Choose Your Stance')
+    cy.get('.form').contains('Choose Your Obstacle')
+  })
+
+  it('should have a button to sumbit your trick', () => {
+    cy.get(".button").click()
+  })
+
+  it('should reflect the data in the form input', () => {
+    cy.get('Choose Your Stance').get('Regular').contains('Regular')
+  })
+
+})
