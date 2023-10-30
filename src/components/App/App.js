674892bcaf988410d60046c1d66fdbab8e271654
Jan McSorley <mcsorleyjan@gmail.com>
2023-10-30 13:10:31 -0500

    Add functionality for iteration 2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import Tricks from '../Tricks/Tricks';
+import Form from '../Form/Form';
 import { getAllTricks } from '../../apiCalls';
 import { useState, useEffect } from 'react';
 import React from 'react';
-
 
 function App() {
   const [tricks, setTricks] = useState([]);
@@ -11,16 +11,21 @@ function App() {
 
   useEffect(() => {
     getAllTricks()
-    .then(data => setTricks(data))
-    .catch(error => setError(`${error.message}: Oops! Something went wrong. Please try again`))
-  })
+      .then((data) => setTricks(data))
+      .catch((error) =>
+        setError(
+          `${error.message}: Oops! Something went wrong. Please try again`
+        )
+      );
+  });
 
   return (
     <div className="App">
       <h1>Sick Trick Wish List</h1>
+      <Form />
       <Tricks tricks={tricks} />
     </div>
   );
 }
 
-export default App; 
+export default App;
---
+++ src/components/Form/Form.js
@@ -0,0 +1,79 @@
+import './Form.css';
+import { useState } from 'react';
+
+function Form ( {addTrick} ) {
+    const [stance, setStance] = useState('');
+    const [name, setName] = useState('');
+    const [obstacle, setObstacle] = useState('');
+    const [tutorial, setTutorial] = useState('');
+
+    function handleClick(event) {
+        event.preventDefault();
+        const newTrick = {
+            id: Date.now(),
+            stance,
+            name,
+            obstacle,
+            tutorial
+        };
+        addTrick(newTrick);
+        clearForm();
+    }
+
+    function clearForm() {
+        setStance('choose-stance');
+        setName('');
+        setObstacle('choose-obstacle');
+        setTutorial('');
+    }
+
+    return (
+    <form>
+        <label htmlFor="stance">
+            <select
+            name="stance"
+            id="stance"
+            onChange={(event) => setStance(event.target.value)}>
+                <option value="choose-stance">Choose your Stance</option>
+                <option value="regular">Regular</option>
+                <option value="switch">Switch</option>
+            </select>
+        </label>
+        <label htmlFor="name">
+            <input
+            type="text"
+            name="name"
+            id="name"
+            value={name}
+            placeholder="Name of Trick"
+            onChange={(event) => setName(event.target.value)} />
+        </label>
+        <label htmlFor="obstacle">
+            <select
+            name="obstacle"
+            id="obstacle"
+            onChange={(event) => setObstacle(event.target.value)}>
+                <option value="choose-obstacle">Choose your Obstacle</option>
+                <option value="flatground">Flatground</option>
+                <option value="ledge">Ledge</option>
+                <option value="rail">Rail</option>
+                <option value="stairs">Stairs</option>
+                <option value="pool">Pool</option>
+            </select>
+            </label>
+        <label htmlFor="tutorial">
+            <input
+            type="text"
+            name="tutorial"
+            id="tutorial"
+            value={tutorial}
+            placeholder="Link to Tutorial"
+            onChange={(event) => setTutorial(event.target.value)} />
+        </label>
+        <button onClick={(event) => handleClick(event)}>Send it!</button>
+    </form>
+    );
+}
+
+export default Form;
+
