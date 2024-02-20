70f9164d7f090c880f69857c3eb85177d3b6c4e7
Matt Garcia <MattpGarcia13@users.noreply.github.com>
2024-02-20 10:55:38 -0700

    Added functional fetch req

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,41 @@
 import './App.css';
 import React from 'react';
+import { useEffect, useState } from 'react';
 
 
 
 function App() {
+  const dummyData = [
+    {
+      "stance": "regular",
+      "name": "treflip",
+      "obstacle": "flat ground",
+      "tutorial": "https://www.youtube.com/watch?v=XGw3YkQmNig",
+      "id": 1
+    },
+    {
+      "stance": "switch",
+      "name": "heelflip",
+      "obstacle": "stairs",
+      "tutorial": "https://www.youtube.com/watch?v=9N9swrZU1HA",
+      "id": 2
+    },
+    {
+      "stance": "regular",
+      "name": "frontside 50-50, backside 180 out",
+      "obstacle": "ledge",
+      "tutorial": "https://www.youtube.com/watch?v=9N9swrZU1HA",
+      "id": 3
+    }
+  ]
 
+  useEffect(() => {
+    fetch('http://localhost:3001/api/v1/tricks')
+      .then(responce => responce.json())
+      .then(data => console.log(data))
+      .catch(error => console.log(error))
+  }, [])
 
-
-  
   return (
     <div className="App">
       <h1>Sick Trick Wish List</h1>
